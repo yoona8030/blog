fbab81358e5ffa431e497c1df744e4e8174eb185
ya8030 <ya8030@naver.com>
2024-12-20 16:44:35 +0900

    Django Project

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post = published_date = timezone.now()
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -18,7 +18,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form':form})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -37,4 +37,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit,html', {'form':form})
+    return render(request, 'blog/post_edit.html', {'form':form})
